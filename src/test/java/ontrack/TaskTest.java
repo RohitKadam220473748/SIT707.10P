53ea2b506e457e82e49912ed4f543480d8e1fce2
ROHIT KADAM <rohitkadam680@gmail.com>
2024-05-17 13:12:44 +1000

    done

--- src/test/java/ontrack/TaskTest.java
+++ src/test/java/ontrack/TaskTest.java
@@ -22,4 +22,17 @@ public class TaskTest {
         System.out.println("Timestamp: " + timestamp);
     }
 
+	@Test
+    public void testfailAddCommentAndPrintTimestamp() {
+        Task task = new Task("Task 1");
+        task.addComment("Hi Ma'am please check my task");
+        List<String> comments = task.getComments();
+        assertEquals(1, comments.size());
+        assertEquals("Please check my task", comments.get(0));
+        System.out.println("Task Message: " + comments);
+
+        assertNotNull(task.getTimestamps().get(0));
+        LocalDateTime timestamp = task.getTimestamps().get(0);
+        System.out.println("Timestamp: " + timestamp);
+    }
 }
